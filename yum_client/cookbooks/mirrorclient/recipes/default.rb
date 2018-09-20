90a09cbcc9ab1b186a4714608615c9affcd432e9
abc1196 <alejob_11@hotmail.com>
2018-09-20 17:52:33 -0500

    Initial commit

---
+++ yum_client/cookbooks/mirrorclient/recipes/default.rb
@@ -0,0 +1,4 @@
+include_recipe 'mirrorclient::mirror_hosts'
+include_recipe 'mirrorclient::mirror_delete_repos'
+include_recipe 'mirrorclient::mirror_repo'
+include_recipe 'mirrorclient::mirror_yum'
---
+++ yum_server/cookbooks/mirrorserver/recipes/mirrorserver_install.rb
@@ -0,0 +1,14 @@
+bash 'mirrorserver_install' do
+  user 'root'
+  code <<-EOH
+  yum install -y createrepo
+  yum install -y yum-plugin-downloadonly
+  yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+  yum install --downloadonly --downloaddir=/var/repo python36
+  yum install --downloadonly --downloaddir=/var/repo python36u-pip
+  createrepo /var/repo/
+  ln -s /var/repo /var/www/html/repo
+  yum -y install policycoreutils-python
+  semanage fcontext -a -t httpd_sys_content_t "/var/repo(/.*)?" && restorecon -rv /var/repo
+  EOH
+end
