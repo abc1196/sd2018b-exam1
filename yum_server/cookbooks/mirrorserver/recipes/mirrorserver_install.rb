ba7c62daf6164d912f0e11d6d98d25426f0c70a9
abc1196 <alejob_11@hotmail.com>
2018-09-21 17:17:47 -0500

    Added endpoint

--- yum_server/cookbooks/mirrorserver/recipes/mirrorserver_install.rb
+++ yum_server/cookbooks/mirrorserver/recipes/mirrorserver_install.rb
@@ -4,8 +4,10 @@ bash 'mirrorserver_install' do
   yum install -y createrepo
   yum install -y yum-plugin-downloadonly
   yum install -y https://centos7.iuscommunity.org/ius-release.rpm
-  yum install --downloadonly --downloaddir=/var/repo python36
+  yum install --downloadonly --downloaddir=/var/repo epel-release
+  yum install --downloadonly --downloaddir=/var/repo python36u
   yum install --downloadonly --downloaddir=/var/repo python36u-pip
+  yum install --downloadonly --downloaddir=/var/repo wget
   createrepo /var/repo/
   ln -s /var/repo /var/www/html/repo
   yum -y install policycoreutils-python
